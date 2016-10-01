7f3e0288eacee2120901823cf613cedbc3341c4d
Angel Cellillo <angelcellillo@gmail.com>
2016-10-01 11:24:22 -0300

    Se agrega mensaje de bienvenida al sistema

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
+void mostrarMensajeBienvenida()
+{
+        printf("Bienvenido al sistema.");
+}
+
 void main()
 {
         printf("Hola Carola");
+		mostrarMensajeBienvenida();
 }
